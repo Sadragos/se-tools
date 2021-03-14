8cc3e86c4a88ec7e904bc857f46d1a64586b3c98
Sadragos <sadragos@13byte.de>
2021-03-14 11:02:20 +0100

    bundled textcon.exe into jar

--- src/eu/fehlemann/ImageConverter.java
+++ src/eu/fehlemann/ImageConverter.java
@@ -2,27 +2,40 @@ package eu.fehlemann;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 
 public class ImageConverter {
     public static final File TEXTCONV_PATH = new File("texconv.exe");
 
     public static void convertFolder(File folder) throws IOException, InterruptedException {
-        for(File f : ArrayUtils.ArrayToList(folder.listFiles((file, s) -> s.toLowerCase().endsWith("dds") || s.toLowerCase().endsWith("png")))) {
+        for (File f : ArrayUtils.ArrayToList(folder.listFiles((file, s) -> s.toLowerCase().endsWith("dds") || s.toLowerCase().endsWith("png")))) {
             convertFile(f);
+        }
+        if (TEXTCONV_PATH.exists()) {
+            TEXTCONV_PATH.delete();
         }
     }
 
     public static void convertFile(File file) throws IOException, InterruptedException {
+        if (!TEXTCONV_PATH.exists()) {
+            RessourceCopy ressourceCopy = new RessourceCopy();
+            try {
+                ressourceCopy.copyFromJar("assets/texconv.exe", TEXTCONV_PATH.toPath());
+            } catch (URISyntaxException e) {
+                System.out.println("Could not extract textconv.exe");
+            }
+        }
         ProcessBuilder processBuilder = null;
         boolean toDDS = false;
-        if(file.getName().toLowerCase().endsWith("dds")) {
+        if (file.getName().toLowerCase().endsWith("dds")) {
             processBuilder = new ProcessBuilder(TEXTCONV_PATH.getAbsolutePath(), "-ft", "png", file.getAbsolutePath(), "-y");
         } else if (file.getName().toLowerCase().endsWith("png")) {
             toDDS = true;
             processBuilder = new ProcessBuilder(TEXTCONV_PATH.getAbsolutePath(), "-f", "BC7_UNORM", file.getAbsolutePath(), "-y");
         }
 
-        if(processBuilder != null) {
+        if (processBuilder != null) {
             System.out.print("Converting " + file.getName() + "... ");
             processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
             processBuilder.directory(file.getParentFile());
@@ -31,7 +44,7 @@ public class ImageConverter {
             if (waitFlag == 0) {
                 System.out.println("ok");
                 String blankName = file.getName().substring(0, file.getName().indexOf("."));
-                if(toDDS) {
+                if (toDDS) {
                     File badName = new File(file.getParent(), blankName + ".DDS");
                     File goodName = new File(file.getParent(), blankName + ".dds");
                     if (badName.exists() && badName.renameTo(goodName)) {
@@ -40,12 +53,10 @@ public class ImageConverter {
                 } else {
                     File badName = new File(file.getParent(), blankName + ".PNG");
                     File goodName = new File(file.getParent(), blankName + ".png");
-                    if(badName.exists() && badName.renameTo(goodName)) {
+                    if (badName.exists() && badName.renameTo(goodName)) {
                         System.out.println("Corrected Filename " + badName.getName() + " -> " + goodName.getName());
                     }
                 }
-
-
             } else {
                 System.out.println("error");
             }
---
+++ src/eu/fehlemann/RessourceCopy.java
@@ -0,0 +1,48 @@
+package eu.fehlemann;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
+
+public class RessourceCopy {
+    public void copyFromJar(String source, final Path target) throws URISyntaxException, IOException {
+        URI resource = getClass().getResource("").toURI();
+        FileSystem fileSystem = null;
+        try {
+            fileSystem = FileSystems.getFileSystem(resource);
+        } catch (Exception e) {
+            // Nothing
+        }
+        if(fileSystem == null) {
+            fileSystem = FileSystems.newFileSystem(
+                    resource,
+                    Collections.<String, String>emptyMap()
+            );
+        }
+
+
+        final Path jarPath = fileSystem.getPath(source);
+
+        Files.walkFileTree(jarPath, new SimpleFileVisitor<Path>() {
+
+            private Path currentTarget;
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                currentTarget = target.resolve(jarPath.relativize(dir).toString());
+                Files.createDirectories(currentTarget);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.copy(file, target.resolve(jarPath.relativize(file).toString()), StandardCopyOption.REPLACE_EXISTING);
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
+    }
+}
